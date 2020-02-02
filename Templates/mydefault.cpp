796e656a6848b8ddb36b532767a4e09e79d88b2d
SmitMangukiya <43950077+SmitMangukiya@users.noreply.github.com>
2020-02-03 03:37:17 +0530

    Create mydefault.cpp

---
+++ Templates/mydefault.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define f(i,a,n) for(int i=a ; i<n ; i++)
+#define all(c) (c).begin(),(c).end()
+#define F first
+#define S second
+#define fast ios:: sync_with_stdio(false),cin.tie(nullptr);
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef pair<pii,int> ppi;
+typedef vector<pii> vpi;
+
+const int inf = 1e9;
+const int inf64 = 1e18;
+const int MOD = inf + 7;
+const ld PI = acos(-1.0);
+
+int32_t main(){
+	fast;
+
+}
